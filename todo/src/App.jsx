2ca6b7f8278ef2c5060244d77d4ed0d2efdb9ead
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2025-01-14 16:28:15 -0300

    Adicionei atualizações no TodoForm e App.jsx
    adicionei a função que limpa os campos após inserir os valores, alem de mostrar na pagina os valores adicionados

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -5,7 +5,6 @@ import TodoForm from "./components/TodoForm";
 
 import "./App.css";
 
-
 function App() {
   const [todos, setTodos] = useState([
     //Todos Consulta os dados, ja o SetTodos insere os Dados
@@ -29,16 +28,29 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1> Lista de Tarefas </h1>
       <hr />
       <div className="todo_list">
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
